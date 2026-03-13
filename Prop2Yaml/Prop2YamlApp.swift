05e2f2aa2a34a7464dcba593f15bbf248d10fce4
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2026-03-13 12:03:02 +0100

    Initial Commit

---
+++ Prop2Yaml/Prop2YamlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prop2YamlApp.swift
+//  Prop2Yaml
+//
+//  Created by Vladimir Kolev on 13.03.26.
+//
+
+import SwiftUI
+
+@main
+struct Prop2YamlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
